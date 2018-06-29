4a7b5e750e80f9768d30ce49b719d94557953d08
ananjaser1211 <ananjaser@gmail.com>
2018-06-30 03:18:11 +0400

    Ramdisk: Add Helios Ramdisk Merged for Oreo Dexopt Patched Sepolicy Patched for deodex/denaials @mwilky AutoReboot script Fake_knox Basic Tweaks

---
+++ Helios/Ramdisk/ramdisk/sbin/init.dalvik.sh
@@ -0,0 +1,38 @@
+#!/system/bin/sh
+# mwilky
+
+LOGFILE=/data/dalvik.log
+REBOOTLOGFILE=/data/reboot.log
+
+rm $LOGFILE
+
+log_print() {
+  echo "$1"
+  echo "$1" >> $LOGFILE
+}
+
+rebootlog_print() {
+  echo "$1"
+  echo "$1" >> $REBOOTLOGFILE
+}
+
+log_print "**RENOVATE dalvik script $( date +"%m-%d-%Y %H:%M:%S" )**"
+
+if [ -f /data/magisk.apk ]; then
+	pm install /data/magisk.apk
+	rm /data/magisk.apk
+else 
+if [ -f /data/adb/magisk/magisk.apk ]; then
+	mv /data/adb/magisk/magisk.apk /data/magisk.apk
+	pm install -r /data/magisk.apk
+	rm /data/magisk.apk
+fi
+fi
+
+if [ -z "$(ls -A /data/dalvik-cache/arm64)" ]; then
+   rebootlog_print "dalvik cache not built, rebooted at $( date +"%m-%d-%Y %H:%M:%S" )"
+   reboot
+else
+   log_print "dalvik cache already built, nothing to do"
+   exit 0
+fi
