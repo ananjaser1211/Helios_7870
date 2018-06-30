bb5a917c13c9f7f07a8fe2e198b694c7fb1cceae
ananjaser1211 <ananjaser@gmail.com>
2018-06-30 06:44:05 +0400

    ramdisk: init.d support

---
+++ Helios/Ramdisk/ramdisk/sbin/sysinit.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+mount -o remount,rw /;
+mount -o rw,remount /system
+
+# init.d support
+if [ ! -e /system/etc/init.d ]; then
+	mkdir /system/etc/init.d
+	chown -R root.root /system/etc/init.d
+	chmod -R 755 /system/etc/init.d
+fi
+
+# start init.d
+for FILE in /system/etc/init.d/*; do
+	sh $FILE >/dev/null
+done;
