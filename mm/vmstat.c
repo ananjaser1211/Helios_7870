49aa6777bee936101a2ce3647ef192601fd896fc
ananjaser1211 <ananjaser@gmail.com>
2018-08-23 14:36:27 +0400

    SQUASH: UKSM: Version 0.1.2.3 for Linux 3.18.y
    Signed-off-by: morogoku <morogoku@hotmail.com>
    
    UKSM: remove warnings
    
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    
    UKMS: disable by default
    
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -797,6 +797,9 @@ const char * const vmstat_text[] = {
 	"nr_anon_transparent_hugepages",
 	"nr_free_cma",
 
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
