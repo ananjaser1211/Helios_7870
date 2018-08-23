0a4dc631232579ea5d2239f41a0acf6674b1906c
ananjaser1211 <ananjaser@gmail.com>
2018-08-23 14:36:27 +0400

    SQUASH: UKSM: Version 0.1.2.3 for Linux 3.18.y
    Signed-off-by: morogoku <morogoku@hotmail.com>
    
    UKSM: remove warnings
    
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    
    UKMS: disable by default
    
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -161,6 +161,9 @@ enum zone_stat_item {
 	WORKINGSET_NODERECLAIM,
 	NR_ANON_TRANSPARENT_HUGEPAGES,
 	NR_FREE_CMA_PAGES,
+#ifdef CONFIG_UKSM
+	NR_UKSM_ZERO_PAGES,
+#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 /*
@@ -872,7 +875,7 @@ static inline int is_highmem_idx(enum zone_type idx)
 }
 
 /**
- * is_highmem - helper function to quickly check if a struct zone is a 
+ * is_highmem - helper function to quickly check if a struct zone is a
  *              highmem zone or not.  This is an attempt to keep references
  *              to ZONE_{DMA/NORMAL/HIGHMEM/etc} in general code to a minimum.
  * @zone - pointer to struct zone variable
