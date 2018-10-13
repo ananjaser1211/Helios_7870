0494f11c646c0f8568ecf4eb389d939d274b337b
renoxtv <guyguygftyfty@gmail.com>
2018-10-14 03:44:19 +0400

    sched: fair: Reduce latency and granularity

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -60,8 +60,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 6000000ULL;
-unsigned int normalized_sysctl_sched_latency = 6000000ULL;
+unsigned int sysctl_sched_latency = 4000000ULL;
+unsigned int normalized_sysctl_sched_latency = 4000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
@@ -79,8 +79,8 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
  * Minimal preemption granularity for CPU-bound tasks:
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 750000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 750000ULL;
+unsigned int sysctl_sched_min_granularity = 550000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 550000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
