d7e6a58fd7654e43571b7f5fb13d2730a1638699
ananjaser1211 <ananjaser@gmail.com>
2018-10-27 14:17:35 +0400

    cronos: J710X

--- cronos.sh
+++ cronos.sh
@@ -48,7 +48,7 @@ CR_CONFG_J530F=j5y17lte_2G_defconfig
 CR_VARIANT_J530F=J530F_2GB
 # Device specific Variables [SM-J530_3GB (Y/YM/FM/GM)]
 CR_DTSFILES_J530M="exynos7870-j5y17lte_sea_openm_03.dtb exynos7870-j5y17lte_sea_openm_05.dtb exynos7870-j5y17lte_sea_openm_07.dtb"
-CR_CONFG_J530M=j5y17lte_3G_defconfig
+CR_CONFG_J530M=j5y17lte_2G_defconfig
 CR_VARIANT_J530M=J530Y_3GB
 # Device specific Variables [SM-J730F/G]
 CR_DTSFILES_J730F="exynos7870-j7y17lte_eur_open_00.dtb exynos7870-j7y17lte_eur_open_01.dtb exynos7870-j7y17lte_eur_open_02.dtb exynos7870-j7y17lte_eur_open_03.dtb exynos7870-j7y17lte_eur_open_04.dtb exynos7870-j7y17lte_eur_open_05.dtb exynos7870-j7y17lte_eur_open_06.dtb exynos7870-j7y17lte_eur_open_07.dtb"
