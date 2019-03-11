e9e59a3b529a158c0082d48e3cf7a6ce21f94615
ananjaser1211 <ananjaser@gmail.com>
2019-03-12 00:41:29 +0400

    Cronos script v3

--- cronos.sh
+++ cronos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Cronos Build Script
+# Cronos Build Script V3.0
 # For Exynos7870
-# Coded by BlackMesa/AnanJaser1211 @2018
+# Coded by BlackMesa/AnanJaser1211 @2019
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -16,50 +16,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Directory Contol
+# Main Dir
 CR_DIR=$(pwd)
+# Define toolchan path
 CR_TC=~/Android/Toolchains/linaro-7.4.1-aarch64-linux/bin/aarch64-linux-gnu-
+# Define proper arch and dir for dts files
 CR_DTS=arch/arm64/boot/dts
+# Define boot.img out dir
 CR_OUT=$CR_DIR/Helios/Out
+# Presistant A.I.K Location
 CR_AIK=$CR_DIR/Helios/A.I.K
+# Main Ramdisk Location
 CR_RAMDISK=$CR_DIR/Helios/Ramdisk
 CR_RAMDISK_TREBLE=$CR_DIR/Helios/Treble
+# Compiled image name and location (Image/zImage)
 CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
+# Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/boot.img-dtb
-# Kernel Variables
+# Kernel Name and Version
 CR_VERSION=V2.6
 CR_NAME=HeliosPro_Kernel
+# Thread count
 CR_JOBS=5
+# Target android version and platform (7/n/8/o/9/p)
 CR_ANDROID=o
 CR_PLATFORM=8.0.0
+# Target ARCH
 CR_ARCH=arm64
+# Current Date
 CR_DATE=$(date +%Y%m%d)
 # Init build
 export CROSS_COMPILE=$CR_TC
+# General init
+export ANDROID_MAJOR_VERSION=$CR_ANDROID
+export PLATFORM_VERSION=$CR_PLATFORM
+export $CR_ARCH
 # J710X Specific
 CR_ANDROID_J710X=n
 CR_PLATFORM_J710X=7.0.0
 ##########################################
 # Device specific Variables [SM-J530_2GB (F/G/S/L/K)]
 CR_DTSFILES_J530F="exynos7870-j5y17lte_eur_open_00.dtb exynos7870-j5y17lte_eur_open_01.dtb exynos7870-j5y17lte_eur_open_02.dtb exynos7870-j5y17lte_eur_open_03.dtb exynos7870-j5y17lte_eur_open_05.dtb exynos7870-j5y17lte_eur_open_07.dtb"
-CR_CONFG_J530F=j5y17lte_2G_defconfig
+CR_CONFG_J530F=j5y17lte_defconfig
 CR_VARIANT_J530F=J530F_2GB
 # Device specific Variables [SM-J530_3GB (Y/YM/FM/GM)]
 CR_DTSFILES_J530M="exynos7870-j5y17lte_sea_openm_03.dtb exynos7870-j5y17lte_sea_openm_05.dtb exynos7870-j5y17lte_sea_openm_07.dtb"
-CR_CONFG_J530M=j5y17lte_2G_defconfig
+CR_CONFG_J530M=j5y17lte_defconfig
 CR_VARIANT_J530M=J530Y_3GB
 # Device specific Variables [SM-J730F/G]
 CR_DTSFILES_J730F="exynos7870-j7y17lte_eur_open_00.dtb exynos7870-j7y17lte_eur_open_01.dtb exynos7870-j7y17lte_eur_open_02.dtb exynos7870-j7y17lte_eur_open_03.dtb exynos7870-j7y17lte_eur_open_04.dtb exynos7870-j7y17lte_eur_open_05.dtb exynos7870-j7y17lte_eur_open_06.dtb exynos7870-j7y17lte_eur_open_07.dtb"
-CR_CONFG_J730F=j7y17lte_eur_open_defconfig
+CR_CONFG_J730F=j7y17lte_defconfig
 CR_VARIANT_J730F=J730F-G
 # Device specific Variables [SM-J710X]
 CR_DTSFILES_J710X="exynos7870-j7xelte_eur_open_00.dtb exynos7870-j7xelte_eur_open_01.dtb exynos7870-j7xelte_eur_open_02.dtb exynos7870-j7xelte_eur_open_03.dtb exynos7870-j7xelte_eur_open_04.dtb"
-CR_CONFG_J710X=j7xelte_03_defconfig
+CR_CONFG_J710X=j7xelte_defconfig
 CR_VARIANT_J710X=J710X
 CR_RAMDISK_J710X=$CR_DIR/Helios/Ramdisk_J710X
 # Device specific Variables [SM-G610X]
 CR_DTSFILES_G610X="exynos7870-on7xelte_swa_open_00.dtb exynos7870-on7xelte_swa_open_01.dtb exynos7870-on7xelte_swa_open_02.dtb"
-CR_CONFG_G610X=on7xelteswa_00_defconfig
+CR_CONFG_G610X=on7xelteswa_defconfig
 CR_VARIANT_G610X=G610X
 # Device specific Variables [SM-J600X]
 CR_DTSFILES_J600X="exynos7870-j6lte_ltn_00.dtb exynos7870-j6lte_ltn_02.dtb"
@@ -67,36 +82,23 @@ CR_CONFG_J600X=j6lte_defconfig
 CR_VARIANT_J600X=J600X
 CR_RAMDISK_J600X=$CR_DIR/Helios/Treble
 # Script functions
-CLEAN_SOURCE()
-{
-echo "----------------------------------------------"
-echo " "
-echo "Cleaning"
-#make clean
-#make mrproper
-# rm -r -f $CR_OUT/*
-rm -r -f $CR_DTB
-rm -rf $CR_DTS/.*.tmp
-rm -rf $CR_DTS/.*.cmd
-rm -rf $CR_DTS/*.dtb
-echo " "
-echo "----------------------------------------------"
-}
-DIRTY_SOURCE()
-{
-echo "----------------------------------------------"
-echo " "
-echo "Cleaning"
-# make clean
-# make mrproper
-# rm -r -f $CR_OUT/*
-rm -r -f $CR_DTB
-rm -rf $CR_DTS/.*.tmp
-rm -rf $CR_DTS/.*.cmd
-rm -rf $CR_DTS/*.dtb
-echo " "
-echo "----------------------------------------------"
-}
+
+read -p "Clean source (y/n) > " yn
+if [ "$yn" = "Y" -o "$yn" = "y" ]; then
+     echo "Clean Build"    
+     make clean && make mrproper    
+     rm -r -f $CR_DTB
+     rm -rf $CR_DTS/.*.tmp
+     rm -rf $CR_DTS/.*.cmd
+     rm -rf $CR_DTS/*.dtb      
+else
+     echo "Dirty Build"
+     rm -r -f $CR_DTB
+     rm -rf $CR_DTS/.*.tmp
+     rm -rf $CR_DTS/.*.cmd
+     rm -rf $CR_DTS/*.dtb          
+fi
+
 BUILD_ZIMAGE()
 {
 	echo "----------------------------------------------"
@@ -105,6 +107,11 @@ BUILD_ZIMAGE()
 	export LOCALVERSION=-$CR_NAME-$CR_VERSION-$CR_VARIANT-$CR_DATE
 	make  $CR_CONFG
 	make -j$CR_JOBS
+	if [ ! -e ./arch/arm64/boot/Image ]; then
+	exit 0;
+	echo "zImage Failed to Compile"
+	echo " Abort "
+	fi
 	echo " "
 	echo "----------------------------------------------"
 }
@@ -113,10 +120,8 @@ BUILD_DTB()
 	echo "----------------------------------------------"
 	echo " "
 	echo "Building DTB for $CR_VARIANT"
-	export $CR_ARCH
-	export CROSS_COMPILE=$CR_TC
-	export ANDROID_MAJOR_VERSION=$CR_ANDROID
-	make  $CR_CONFG
+	# Use the DTS list provided in the build script.
+	# This source does not compile dtbs while doing Image
 	make $CR_DTSFILES
 	./scripts/dtbTool/dtbTool -o ./boot.img-dtb -d $CR_DTS/ -s 2048
 	du -k "./boot.img-dtb" | cut -f1 >sizT
@@ -135,25 +140,53 @@ PACK_BOOT_IMG()
 	echo " "
 	echo "Building Boot.img for $CR_VARIANT"
 	cp -rf $CR_RAMDISK/* $CR_AIK
+	# To avoid any permission issues
+	echo "Fix Ramdisk Permissions"
+	cd $CR_RAMDISK
+	find -type d -exec chmod 755 {} \;
+	find -type f -exec chmod 644 {} \;
+	find -name "*.rc" -exec chmod 750 {} \;
+	find -name "*.sh" -exec chmod 750 {} \;
+	chmod -Rf 750 init sbin
+	# Copy Ramdisk
+	cp -rf $CR_RAMDISK/* $CR_AIK
+	# Move Compiled kernel and dtb to A.I.K Folder
 	mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
 	mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
+	# Create boot.img
 	$CR_AIK/repackimg.sh
+	# Remove red warning at boot
 	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
+	# Move boot.img to out dir
 	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT.img
 	$CR_AIK/cleanup.sh
 }
 PACK_BOOT_IMG_TREBLE()
 {
-    echo "----------------------------------------------"
-    echo " "
-    echo "Building Boot.img for $CR_VARIANT"
-    cp -rf $CR_RAMDISK_TREBLE/* $CR_AIK
-    mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
-    mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
-    $CR_AIK/repackimg.sh
-    echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
-    mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT.img
-    $CR_AIK/cleanup.sh
+	echo "----------------------------------------------"
+	echo " "
+	echo "Building Treble Boot.img for $CR_VARIANT"
+	cp -rf $CR_RAMDISK_TREBLE/* $CR_AIK
+	# To avoid any permission issues
+	echo "Fix Ramdisk Permissions"
+	cd $CR_RAMDISK_TREBLE
+	find -type d -exec chmod 755 {} \;
+	find -type f -exec chmod 644 {} \;
+	find -name "*.rc" -exec chmod 750 {} \;
+	find -name "*.sh" -exec chmod 750 {} \;
+	chmod -Rf 750 init sbin
+	# Copy Ramdisk
+	cp -rf $CR_RAMDISK_TREBLE/* $CR_AIK
+	# Move Compiled kernel and dtb to A.I.K Folder
+	mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
+	mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
+	# Create boot.img
+	$CR_AIK/repackimg.sh
+	# Remove red warning at boot
+	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
+	# Move boot.img to out dir
+	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-Treble-$CR_DATE-$CR_VARIANT.img
+	$CR_AIK/cleanup.sh
 }
 # Main Menu
 clear
@@ -167,14 +200,11 @@ do
     case $menuvar in
         "SM-J530_2G")
             clear
-            CLEAN_SOURCE
             echo "Starting $CR_VARIANT_J530F kernel build..."
-	        CR_VARIANT=$CR_VARIANT_J530F
-	        CR_CONFG=$CR_CONFG_J530F
+            CR_VARIANT=$CR_VARIANT_J530F
+            CR_CONFG=$CR_CONFG_J530F
             CR_DTSFILES=$CR_DTSFILES_J530F
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID
-            export PLATFORM_VERSION=$CR_PLATFORM
-	        BUILD_ZIMAGE
+            BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
@@ -182,21 +212,18 @@ do
             echo "$CR_VARIANT kernel build finished."
             echo "$CR_VARIANT Ready at $CR_OUT"
             echo "Combined DTB Size = $sizT Kb"
-	        echo "Press Any key to end the script"
+            echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
             break
             ;;
         "SM-J530_3G")
             clear
-            CLEAN_SOURCE
             echo "Starting $CR_VARIANT_J530M kernel build..."
-	        CR_VARIANT=$CR_VARIANT_J530M
-	        CR_CONFG=$CR_CONFG_J530M
+            CR_VARIANT=$CR_VARIANT_J530M
+            CR_CONFG=$CR_CONFG_J530M
             CR_DTSFILES=$CR_DTSFILES_J530M
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID
-            export PLATFORM_VERSION=$CR_PLATFORM            
-	        BUILD_ZIMAGE
+            BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
@@ -211,14 +238,11 @@ do
             ;;
         "SM-J730F-G")
             clear
-            CLEAN_SOURCE
             echo "Starting $CR_VARIANT_J730F kernel build..."
             CR_VARIANT=$CR_VARIANT_J730F
-	        CR_CONFG=$CR_CONFG_J730F
+            CR_CONFG=$CR_CONFG_J730F
             CR_DTSFILES=$CR_DTSFILES_J730F
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID
-            export PLATFORM_VERSION=$CR_PLATFORM            
-	        BUILD_ZIMAGE
+            BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
@@ -226,20 +250,19 @@ do
             echo "$CR_VARIANT kernel build finished."
             echo "$CR_VARIANT Ready at $CR_OUT"
             echo "Combined DTB Size = $sizT Kb"
-	        echo "Press Any key to end the script"
+            echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
             break
             ;;
         "SM-J710X")
             clear
-            CLEAN_SOURCE
             echo "Starting $CR_VARIANT_J710X kernel build..."
             CR_VARIANT=$CR_VARIANT_J710X
             CR_CONFG=$CR_CONFG_J710X
             CR_DTSFILES=$CR_DTSFILES_J710X
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID
-            export PLATFORM_VERSION=$CR_PLATFORM            
+            export ANDROID_MAJOR_VERSION=$CR_ANDROID_J710X
+            export PLATFORM_VERSION=$CR_PLATFORM_J710X            
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
@@ -255,13 +278,10 @@ do
             ;;
         "SM-J600X")
             clear
-            CLEAN_SOURCE
             echo "Starting $CR_VARIANT_J600X kernel build..."
             CR_VARIANT=$CR_VARIANT_J600X
             CR_CONFG=$CR_CONFG_J600X
-            CR_DTSFILES=$CR_DTSFILES_J600X
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID
-            export PLATFORM_VERSION=$CR_PLATFORM            
+            CR_DTSFILES=$CR_DTSFILES_J600X          
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG_TREBLE
@@ -277,13 +297,10 @@ do
             ;;            
         "SM-G610X")
             clear
-            CLEAN_SOURCE
             echo "Starting $CR_VARIANT_G610X kernel build..."
             CR_VARIANT=$CR_VARIANT_G610X
             CR_CONFG=$CR_CONFG_G610X
             CR_DTSFILES=$CR_DTSFILES_G610X
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID
-            export PLATFORM_VERSION=$CR_PLATFORM            
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
@@ -296,7 +313,7 @@ do
             echo "----------------------------------------------"
             read -n1 -r key
             break
-            ;;            
+            ;;
         "Exit")
             break
             ;;
