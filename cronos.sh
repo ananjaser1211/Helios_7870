0b2e4a706a855a377d8b341c4f52c912e40b6ecc
ananjaser1211 <ananjaser@gmail.com>
2019-04-11 16:08:47 +0400

    cronos: update version

--- cronos.sh
+++ cronos.sh
@@ -34,7 +34,7 @@ CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 # Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/boot.img-dtb
 # Kernel Name and Version
-CR_VERSION=V1.0
+CR_VERSION=V3.0
 CR_NAME=Helios_Pie_Kernel
 # Thread count
 CR_JOBS=5
