113f517e7911496c71cdfd45d53ec22e53cd07ff
ananjaser1211 <ananjaser@gmail.com>
2019-06-15 13:22:50 +0400

    a6lte: revert some stuff to pie

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -53,6 +53,12 @@ static int mmc_queue_thread(void *d)
 {
 	struct mmc_queue *mq = d;
 	struct request_queue *q = mq->queue;
+	struct sched_param scheduler_params = {0};
+
+	if (mq->card && mq->card->type != MMC_TYPE_SD) {
+		scheduler_params.sched_priority = 1;
+		sched_setscheduler(current, SCHED_FIFO, &scheduler_params);
+	}
 
 	current->flags |= PF_MEMALLOC;
 
