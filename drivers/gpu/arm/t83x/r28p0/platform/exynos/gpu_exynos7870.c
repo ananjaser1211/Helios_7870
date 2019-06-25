dccf72fd2e24a2e856ad6a36a229d81492f32b9e
spart4nicus <paradelovhristiyan@gmail.com>
2019-06-25 12:30:57 +0200

    R28P0: gpu_exynos7870: allow maximum clock of 1.3GHz
    Signed-off-by: spart4nicus <paradelovhristiyan@gmail.com>

--- drivers/gpu/arm/t83x/r28p0/platform/exynos/gpu_exynos7870.c
+++ drivers/gpu/arm/t83x/r28p0/platform/exynos/gpu_exynos7870.c
@@ -95,7 +95,7 @@ static int mif_min_table[] = {
 };
 
 static gpu_attribute gpu_config_attributes[] = {
-	{GPU_MAX_CLOCK, 1001},
+	{GPU_MAX_CLOCK, 1300},
 	{GPU_MAX_CLOCK_LIMIT, 1300},
 	{GPU_MIN_CLOCK, 343},
 	{GPU_MIN_CLOCK_LIMIT, 343},
