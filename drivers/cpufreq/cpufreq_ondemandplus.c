12de859adb5a769881286228bdd3ed3a40b4cc4d
spart4nicus <paradelovhristiyan@gmail.com>
2019-06-30 13:23:28 +0200

    cpufreq_ondemandplus: perform optimizations for 7870
    Signed-off-by: spart4nicus <paradelovhristiyan@gmail.com>

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -97,22 +97,22 @@ static unsigned long up_threshold;
 #define DEFAULT_DOWN_DIFFERENTIAL 62
 static unsigned long down_differential;
 
-#define DEFAULT_INTER_HIFREQ 1728000
+#define DEFAULT_INTER_HIFREQ 1794000
 static u64 inter_hifreq;
 
-#define DEFAULT_INTER_LOFREQ 1036800
+#define DEFAULT_INTER_LOFREQ 1144000
 static u64 inter_lofreq;
 
 #define DEFAULT_INTER_STAYCYCLES 2
 static unsigned long inter_staycycles;
 
-#define DEFAULT_STAYCYCLES_RESETFREQ 652800
+#define DEFAULT_STAYCYCLES_RESETFREQ 902000
 static u64 staycycles_resetfreq;
 
 #define DEFAULT_IO_IS_BUSY 0
 static unsigned int io_is_busy;
 
-static u64 screen_on_min_freq = 300000;
+static u64 screen_on_min_freq = 343000;
 
 /*
  * Tunables end
