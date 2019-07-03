fb99cae29033bc211bc780a68e0c832811b57c81
ananjaser1211 <ananjaser@gmail.com>
2019-07-04 00:18:34 +0400

    Helios 3.2 Build script
    - reorganize
    - remove dropped variants
    - rename some variants
    - calculate image and boot size
    - add failsafe for dtb

--- cronos.sh
+++ cronos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Cronos Build Script V3.0
+# Cronos Build Script V3.2
 # For Exynos7870
 # Coded by BlackMesa/AnanJaser1211 @2019
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,14 +27,14 @@ CR_OUT=$CR_DIR/Helios/Out
 # Presistant A.I.K Location
 CR_AIK=$CR_DIR/Helios/A.I.K
 # Main Ramdisk Location
-CR_RAMDISK=$CR_DIR/Helios/Treble_custom
+CR_RAMDISK_PORT=$CR_DIR/Helios/Treble_custom
 CR_RAMDISK_TREBLE=$CR_DIR/Helios/Treble
 # Compiled image name and location (Image/zImage)
 CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 # Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/boot.img-dtb
 # Kernel Name and Version
-CR_VERSION=V3.1
+CR_VERSION=V4.0
 CR_NAME=HeliosPie_Treble
 # Thread count
 CR_JOBS=5
@@ -56,17 +56,17 @@ CR_ANDROID_J710X=n
 CR_PLATFORM_J710X=7.0.0
 ##########################################
 # Device specific Variables [SM-J530_2GB (F/G/S/L/K)]
-CR_DTSFILES_J530F="exynos7870-j5y17lte_eur_open_00.dtb exynos7870-j5y17lte_eur_open_01.dtb exynos7870-j5y17lte_eur_open_02.dtb exynos7870-j5y17lte_eur_open_03.dtb exynos7870-j5y17lte_eur_open_05.dtb exynos7870-j5y17lte_eur_open_07.dtb"
-CR_CONFG_J530F=j5y17lte_defconfig
-CR_VARIANT_J530F=J530X
+CR_DTSFILES_J530X="exynos7870-j5y17lte_eur_open_00.dtb exynos7870-j5y17lte_eur_open_01.dtb exynos7870-j5y17lte_eur_open_02.dtb exynos7870-j5y17lte_eur_open_03.dtb exynos7870-j5y17lte_eur_open_05.dtb exynos7870-j5y17lte_eur_open_07.dtb"
+CR_CONFG_J530X=j5y17lte_defconfig
+CR_VARIANT_J530X=J530X
 # Device specific Variables [SM-J530_3GB (Y/YM/FM/GM)]
 CR_DTSFILES_J530M="exynos7870-j5y17lte_sea_openm_03.dtb exynos7870-j5y17lte_sea_openm_05.dtb exynos7870-j5y17lte_sea_openm_07.dtb"
 CR_CONFG_J530M=j5y17lte_defconfig
 CR_VARIANT_J530M=J530Y
-# Device specific Variables [SM-J730F/G]
-CR_DTSFILES_J730F="exynos7870-j7y17lte_eur_open_00.dtb exynos7870-j7y17lte_eur_open_01.dtb exynos7870-j7y17lte_eur_open_02.dtb exynos7870-j7y17lte_eur_open_03.dtb exynos7870-j7y17lte_eur_open_04.dtb exynos7870-j7y17lte_eur_open_05.dtb exynos7870-j7y17lte_eur_open_06.dtb exynos7870-j7y17lte_eur_open_07.dtb"
-CR_CONFG_J730F=j7y17lte_defconfig
-CR_VARIANT_J730F=J730X
+# Device specific Variables [SM-J730X]
+CR_DTSFILES_J730X="exynos7870-j7y17lte_eur_open_00.dtb exynos7870-j7y17lte_eur_open_01.dtb exynos7870-j7y17lte_eur_open_02.dtb exynos7870-j7y17lte_eur_open_03.dtb exynos7870-j7y17lte_eur_open_04.dtb exynos7870-j7y17lte_eur_open_05.dtb exynos7870-j7y17lte_eur_open_06.dtb exynos7870-j7y17lte_eur_open_07.dtb"
+CR_CONFG_J730X=j7y17lte_defconfig
+CR_VARIANT_J730X=J730X
 # Device specific Variables [SM-J710X]
 CR_DTSFILES_J710X="exynos7870-j7xelte_eur_open_00.dtb exynos7870-j7xelte_eur_open_01.dtb exynos7870-j7xelte_eur_open_02.dtb exynos7870-j7xelte_eur_open_03.dtb exynos7870-j7xelte_eur_open_04.dtb"
 CR_CONFG_J710X=j7xelte_defconfig
@@ -109,11 +109,14 @@ BUILD_ZIMAGE()
 	export LOCALVERSION=-$CR_NAME-$CR_VERSION-$CR_VARIANT-$CR_DATE
 	make  $CR_CONFG
 	make -j$CR_JOBS
-	if [ ! -e ./arch/arm64/boot/Image ]; then
+	if [ ! -e $CR_KERNEL ]; then
 	exit 0;
-	echo "zImage Failed to Compile"
+	echo "Image Failed to Compile"
 	echo " Abort "
 	fi
+    du -k "$CR_KERNEL" | cut -f1 >sizT
+    sizT=$(head -n 1 sizT)
+    rm -rf sizT      
 	echo " "
 	echo "----------------------------------------------"
 }
@@ -125,25 +128,28 @@ BUILD_DTB()
 	# Use the DTS list provided in the build script.
 	# This source does not compile dtbs while doing Image
 	make $CR_DTSFILES
-	./scripts/dtbTool/dtbTool -o ./boot.img-dtb -d $CR_DTS/ -s 2048
-	du -k "./boot.img-dtb" | cut -f1 >sizT
-	sizT=$(head -n 1 sizT)
-	rm -rf sizT
-	echo "Combined DTB Size = $sizT Kb"
+	./scripts/dtbTool/dtbTool -o $CR_DTB -d $CR_DTS/ -s 2048
+    if [ ! -e $CR_DTB ]; then
+    exit 0;
+    echo "DTB Failed to Compile"
+    echo " Abort "
+    fi    
 	rm -rf $CR_DTS/.*.tmp
 	rm -rf $CR_DTS/.*.cmd
 	rm -rf $CR_DTS/*.dtb
+    du -k "$CR_DTB" | cut -f1 >sizdT
+    sizdT=$(head -n 1 sizdT)    
+    rm -rf sizdT
 	echo " "
 	echo "----------------------------------------------"
 }
-PACK_BOOT_IMG()
+PACK_PORT_IMG()
 {
 	echo "----------------------------------------------"
 	echo " "
 	echo "Building Boot.img for $CR_VARIANT"
-	cp -rf $CR_RAMDISK/* $CR_AIK
     # Copy Ramdisk
-    cp -rf $CR_RAMDISK/* $CR_AIK
+    cp -rf $CR_RAMDISK_PORT/* $CR_AIK
 	# Move Compiled kernel and dtb to A.I.K Folder
 	mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
 	mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
@@ -153,14 +159,16 @@ PACK_BOOT_IMG()
 	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
 	# Move boot.img to out dir
 	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img
+    du -k "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img" | cut -f1 >sizkT
+    sizkT=$(head -n 1 sizkT)
+    rm -rf sizkT
 	$CR_AIK/cleanup.sh
 }
-PACK_BOOT_IMG_TREBLE()
+PACK_TREBLE_IMG()
 {
 	echo "----------------------------------------------"
 	echo " "
 	echo "Building Boot.img for $CR_VARIANT"
-	cp -rf $CR_RAMDISK_TREBLE/* $CR_AIK
     # Copy Ramdisk
     cp -rf $CR_RAMDISK_TREBLE/* $CR_AIK
 	# Move Compiled kernel and dtb to A.I.K Folder
@@ -170,8 +178,12 @@ PACK_BOOT_IMG_TREBLE()
 	$CR_AIK/repackimg.sh
 	# Remove red warning at boot
 	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
+    echo " "
 	# Move boot.img to out dir
 	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img
+    du -k "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img" | cut -f1 >sizkT
+    sizkT=$(head -n 1 sizkT)
+    rm -rf sizkT      
 	$CR_AIK/cleanup.sh
 }
 # Main Menu
@@ -180,62 +192,47 @@ echo "----------------------------------------------"
 echo "$CR_NAME $CR_VERSION Build Script"
 echo "----------------------------------------------"
 PS3='Please select your option (1-7): '
-menuvar=("SM-J530_2G" "SM-J530_3G" "SM-J730F-G" "SM-J710X" "SM-J701X" "SM-G610X" "SM-J600X" "Exit")
+menuvar=("SM-J530X" "SM-J730X" "SM-J710X" "SM-J701X" "SM-G610X" "SM-J600X" "Exit")
 select menuvar in "${menuvar[@]}"
 do
     case $menuvar in
-        "SM-J530_2G")
+        "SM-J530X")
             clear
-            echo "Starting $CR_VARIANT_J530F kernel build..."
-            CR_VARIANT=$CR_VARIANT_J530F
-            CR_CONFG=$CR_CONFG_J530F
-            CR_DTSFILES=$CR_DTSFILES_J530F
+            echo "Starting $CR_VARIANT_J530X kernel build..."
+            CR_VARIANT=$CR_VARIANT_J530X
+            CR_CONFG=$CR_CONFG_J530X
+            CR_DTSFILES=$CR_DTSFILES_J530X
             BUILD_ZIMAGE
             BUILD_DTB
-            PACK_BOOT_IMG
+            PACK_PORT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"                         
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
             break
             ;;
-        "SM-J530_3G")
+        "SM-J730X")
             clear
-            echo "Starting $CR_VARIANT_J530M kernel build..."
-            CR_VARIANT=$CR_VARIANT_J530M
-            CR_CONFG=$CR_CONFG_J530M
-            CR_DTSFILES=$CR_DTSFILES_J530M
+            echo "Starting $CR_VARIANT_J730X kernel build..."
+            CR_VARIANT=$CR_VARIANT_J730X
+            CR_CONFG=$CR_CONFG_J730X
+            CR_DTSFILES=$CR_DTSFILES_J730X
             BUILD_ZIMAGE
             BUILD_DTB
-            PACK_BOOT_IMG
+            PACK_PORT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
-            echo "Press Any key to end the script"
-            echo "----------------------------------------------"
-            read -n1 -r key
-            break
-            ;;
-        "SM-J730F-G")
-            clear
-            echo "Starting $CR_VARIANT_J730F kernel build..."
-            CR_VARIANT=$CR_VARIANT_J730F
-            CR_CONFG=$CR_CONFG_J730F
-            CR_DTSFILES=$CR_DTSFILES_J730F
-            BUILD_ZIMAGE
-            BUILD_DTB
-            PACK_BOOT_IMG
-            echo " "
-            echo "----------------------------------------------"
-            echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"                         
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -251,12 +248,14 @@ do
             CR_DTSFILES=$CR_DTSFILES_J710X
             BUILD_ZIMAGE
             BUILD_DTB
-            PACK_BOOT_IMG
+            PACK_PORT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"                         
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -266,18 +265,39 @@ do
             clear
             echo "Starting $CR_VARIANT_J701X kernel build..."
             CR_VARIANT=$CR_VARIANT_J701X
-            export ANDROID_MAJOR_VERSION=$CR_ANDROID_J710X
-            export PLATFORM_VERSION=$CR_PLATFORM_J710X
             CR_CONFG=$CR_CONFG_J701X
             CR_DTSFILES=$CR_DTSFILES_J701X
+            BUILD_ZIMAGE
+            BUILD_DTB
+            PACK_PORT_IMG
+            echo " "
+            echo "----------------------------------------------"
+            echo "$CR_VARIANT kernel build finished."
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"                         
+            echo "Press Any key to end the script"
+            echo "----------------------------------------------"
+            read -n1 -r key
+            break
+            ;;
+        "SM-G610X")
+            clear
+            echo "Starting $CR_VARIANT_G610X kernel build..."
+            CR_VARIANT=$CR_VARIANT_G610X
+            CR_CONFG=$CR_CONFG_G610X
+            CR_DTSFILES=$CR_DTSFILES_G610X
             BUILD_ZIMAGE
             BUILD_DTB
             PACK_BOOT_IMG
             echo " "
             echo "----------------------------------------------"
             echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"                         
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
@@ -293,36 +313,19 @@ do
             export PLATFORM_VERSION=$CR_PLATFORM            
             BUILD_ZIMAGE
             BUILD_DTB
-            PACK_BOOT_IMG_TREBLE
+            PACK_TREBLE_IMG
             echo " "
             echo "----------------------------------------------"
-            echo "$CR_VARIANT_J600X kernel build finished."
-            echo "$CR_VARIANT_J600X Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
+            echo "$CR_VARIANT kernel build finished."
+            echo "Compiled DTB Size = $sizdT Kb"
+            echo "Kernel Image Size = $sizT Kb"
+            echo "Boot Image   Size = $sizkT Kb"
+            echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"                         
             echo "Press Any key to end the script"
             echo "----------------------------------------------"
             read -n1 -r key
             break
             ;;            
-        "SM-G610X")
-            clear
-            echo "Starting $CR_VARIANT_G610X kernel build..."
-            CR_VARIANT=$CR_VARIANT_G610X
-            CR_CONFG=$CR_CONFG_G610X
-            CR_DTSFILES=$CR_DTSFILES_G610X
-            BUILD_ZIMAGE
-            BUILD_DTB
-            PACK_BOOT_IMG
-            echo " "
-            echo "----------------------------------------------"
-            echo "$CR_VARIANT kernel build finished."
-            echo "$CR_VARIANT Ready at $CR_OUT"
-            echo "Combined DTB Size = $sizT Kb"
-            echo "Press Any key to end the script"
-            echo "----------------------------------------------"
-            read -n1 -r key
-            break
-            ;;
         "Exit")
             break
             ;;
