f2af58b3ee2f94bcbb208e235013fbd1f6f7e700
spart4nicus <paradelovhristiyan@gmail.com>
2019-07-04 11:27:49 +0200

    Revert "zswap: re-check zswap_is_full() after do zswap_shrink()"
    This reverts commit 50aad5ab580d4dd475ac1157eba38a79010547c2.

--- mm/zswap.c
+++ mm/zswap.c
@@ -862,20 +862,8 @@ static int zswap_frontswap_store(unsigned type, pgoff_t offset,
 
 	if (zswap_is_full(ZSWAP_POOL_MAX)) {
 		zswap_pool_limit_hit++;
-		if (zpool_shrink(zswap_pool, 1, NULL)) {
-			zswap_reject_reclaim_fail++;
-			ret = -ENOMEM;
-			goto reject;
-		}
-
-		/* A second zswap_is_full() check after
-		 * zswap_shrink() to make sure it's now
-		 * under the max_pool_percent
-		 */
-		if (zswap_is_full()) {
-			ret = -ENOMEM;
-			goto reject;
-		}
+		ret = -ENOMEM;
+		goto reject;
 	}
 
 	/* allocate entry */
